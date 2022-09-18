9b851b504aebb0667247ada38b429234123d0ec1
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-09-18 11:59:34 -0300

    WIP

--- cmd/action_setup.go
+++ cmd/action_setup.go
@@ -1,11 +1,28 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"fmt"
+	"os"
 
-var (
-	
+	"github.com/guionardo/todo-cli/internal"
+	"github.com/urfave/cli/v2"
 )
 
+var ()
+
+func getUser() string {
+	gitUser, err := internal.GetCurrentGitUser()
+	if err == nil && len(gitUser.Name) > 0 {
+		return gitUser.Name
+	}
+	user := os.Getenv("USER")
+	if len(user) == 0 {
+		user = "UNDEFINED"
+	}
+	return user
+}
 func ActionSetup(c *cli.Context) error {
+	fmt.Println("Starting todo setup")
+	default
 	return nil
 }
